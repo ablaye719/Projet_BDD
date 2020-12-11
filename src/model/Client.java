b67c5e73f14b9111949886888b3d487c524dc310
Ablaye Sandiara Sow <sowablayesandiara@gmail.com>
2020-12-11 14:21:45 +0100

    modele client

---
+++ src/model/Client.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Client extends Entity{
+    private int idClient;
+    private String nomClient;
+    private String adresseClient;
+    private String codePostalClient;
+    private int idVille;
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public String getNomClient() {
+        return nomClient;
+    }
+
+    public void setNomClient(String nomClient) {
+        this.nomClient = nomClient;
+    }
+
+    public String getAdresseClient() {
+        return adresseClient;
+    }
+
+    public void setAdresseClient(String adresseClient) {
+        this.adresseClient = adresseClient;
+    }
+
+    public String getCodePostalClient() {
+        return codePostalClient;
+    }
+
+    public void setCodePostalClient(String codePostalClient) {
+        this.codePostalClient = codePostalClient;
+    }
+
+    public int getIdVille() {
+        return idVille;
+    }
+
+    public void setIdVille(int idVille) {
+        this.idVille = idVille;
+    }
+}
